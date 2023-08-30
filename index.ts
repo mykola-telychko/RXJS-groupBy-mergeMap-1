3179c45ac71a7ce497d477d0a99c3f1e8bb1ee67
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-31 01:24:35 +0300

    fin

--- index.ts
+++ index.ts
@@ -11,17 +11,18 @@ const people = [
   { name: 'Sarah', age: 35 },
 ];
 //emit each person
-const source = from(people);
+const $src = from(people);
 //group by age
-const example = source.pipe(
+const example = $src.pipe(
   groupBy((person) => person.age),
   // return each item in group as array
   mergeMap((group) => group.pipe(toArray()))
 );
+const subscribe = example.subscribe((val) => console.log(val));
+
 /*
   output:
   [{age: 25, name: "Sue"},{age: 25, name: "Frank"}]
   [{age: 30, name: "Joe"}]
   [{age: 35, name: "Sarah"}]
 */
-const subscribe = example.subscribe((val) => console.log(val));
